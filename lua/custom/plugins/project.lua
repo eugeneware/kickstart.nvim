fa6f036b546786ff0ae63674e9744d008fc9a741
Eugene Ware <eware@godaddy.com>
2024-04-26 09:46:03 +0000

    Plugin to manage project folders

---
+++ lua/custom/plugins/project.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'ahmedkhalf/project.nvim',
+    config = function()
+      local ts = require 'telescope'
+      ts.load_extension 'projects'
+      vim.keymap.set('n', '<leader>p', ts.extensions.projects.projects, { desc = 'Pick projects' })
+      require('project_nvim').setup {}
+    end,
+  },
+}
