a133650970178418708f2e2fdfd57fa2596ccf9e
Eugene Ware <eware@godaddy.com>
2024-05-14 16:32:32 +1000

    add <leader>xx to send code cell

--- lua/custom/plugins/slimux.lua
+++ lua/custom/plugins/slimux.lua
@@ -6,6 +6,40 @@ return {
 
     local map = vim.api.nvim_set_keymap
 
+    function select_code_cell()
+      -- Search backwards for the cell delimiter from the current line.
+      local start = vim.fn.search('# %%', 'bW')
+
+      -- If the start was found, move to the next line to start the selection.
+      -- Otherwise, move to the first line of the buffer.
+      if start > 0 then
+        vim.cmd 'normal! j'
+      else
+        vim.cmd 'normal! gg'
+      end
+
+      -- Start visual mode.
+      vim.cmd 'normal! V'
+
+      -- Search forwards for the next cell delimiter.
+      local finish = vim.fn.search('# %%', 'W')
+
+      -- If the end was found, move up one line to end the selection.
+      -- Otherwise, move to the last line of the buffer.
+      if finish > 0 then
+        vim.cmd 'normal! k'
+      else
+        vim.cmd 'normal! G'
+      end
+    end
+
+    vim.api.nvim_set_keymap(
+      'n',
+      '<Leader>xx',
+      ':lua select_code_cell()<CR>:SlimuxREPLSendSelection<CR>',
+      { noremap = true, silent = true, desc = 'Send code cell' }
+    )
+
     map('n', '<leader>xs', ':SlimuxREPLSendLine<CR>', { noremap = true, silent = true, desc = 'Send line to REPL' })
     map('n', '<leader>xp', ':SlimuxREPLSendParagraph<CR>', { noremap = true, silent = true, desc = 'Send paragraph to REPL' })
     map('v', '<leader>xs', ':SlimuxREPLSendSelection<CR>', { noremap = true, silent = true, desc = 'Send selection to REPL' })
