0425ec5e14419dfdb58138c52bcdb2c6239ebf91
Eugene Ware <eware@godaddy.com>
2025-03-19 20:02:09 +1100

    only detect projectd root based on .git

--- lua/custom/plugins/project.lua
+++ lua/custom/plugins/project.lua
@@ -2,13 +2,15 @@ return {
   {
     'ahmedkhalf/project.nvim',
     config = function()
-      local ts = require 'telescope'
-      ts.load_extension 'projects'
-      vim.keymap.set('n', '<leader>p', ts.extensions.projects.projects, { desc = 'Pick projects' })
       require('project_nvim').setup {
         scope_chdir = 'tab',
         silent_chdir = false,
+        detection_methods = { 'pattern' },
+        patterns = { '.git' },
       }
+      local ts = require 'telescope'
+      ts.load_extension 'projects'
+      vim.keymap.set('n', '<leader>p', ts.extensions.projects.projects, { desc = 'Pick projects' })
     end,
   },
 }
