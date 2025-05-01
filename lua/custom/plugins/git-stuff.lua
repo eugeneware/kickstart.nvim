4c4dc47338910514ac777d2c2fbed16bd9c97d73
Eugene Ware <eware@godaddy.com>
2025-05-01 10:53:50 +1000

    <leader>gs for git status telescope

--- lua/custom/plugins/git-stuff.lua
+++ lua/custom/plugins/git-stuff.lua
@@ -7,6 +7,13 @@ return {
 
       vim.keymap.set('n', "[c", ":Gitsigns nav_hunk prev<CR>", { desc = 'Previous [C]ommit hunk' })
       vim.keymap.set('n', "]c", ":Gitsigns nav_hunk next<CR>", { desc = 'Next [C]ommit hunk' })
+
+      local ok, builtin = pcall(require, 'telescope.builtin')
+      if ok then
+        vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = '[G]it [S]tatus (Telescope)' })
+        vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = '[G]it [C]ommits (Telescope)' })
+      end
+
     end,
   },
   {
