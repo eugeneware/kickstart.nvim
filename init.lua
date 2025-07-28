8b3948163eaf22effb76103d4f48c3049ec29c2d
Eugene Ware <eware@godaddy.com>
2025-07-28 17:15:27 +1000

    fix issue with gitcommit files not working with neovim with git commit -a

--- init.lua
+++ init.lua
@@ -887,7 +887,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'vim', 'vimdoc', 'gitcommit' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
